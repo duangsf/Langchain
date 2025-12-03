456ca3599f09761cf347160d92789794a7077868
duangsf <552077267@qq.com>
2025-12-03 15:21:00 +0800

    test

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-1231231123123123123
+from langchain_deepseek import ChatDeepSeek
+from langchain_openai import ChatOpenAI
+
+from env_utils import ALIBABA_API_KEY, ALIBABA_BASE_URL
+
+#调用阿里云百炼的DeepSeek模型   langchain-OpenAI的插件 只能看到模型输出，不能看到模型思考的过程
+# llm = ChatOpenAI(
+#     model_name="deepseek-r1-0528",
+#     temperature=0.5,  #模型随机性，越低越保守和重复，越接近1越有创意
+#     api_key=ALIBABA_API_KEY,
+#     base_url=ALIBABA_BASE_URL
+# )
+
+#使用的langchain-deepseek   调用时可以看到模型思考的过程
+llm = ChatDeepSeek(
+    model_name="deepseek-r1-0528",
+    temperature=0.5,  #模型随机性，越低越保守和重复，越接近1越有创意
+    api_key=ALIBABA_API_KEY,
+    api_base=ALIBABA_BASE_URL
+)
+
---
+++ test_llm/test_01.py
@@ -0,0 +1,5 @@
+from test import llm
+
+resp = llm.invoke("请用三句话介绍 yourself")
+print(type(resp))
+print(resp)
