16c11aee6fc29ade789a84ae52a1c29f5b36eb5f
duangsf <552077267@qq.com>
2025-12-03 19:21:14 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_deepseek import ChatDeepSeek
 from langchain_openai import ChatOpenAI
 
@@ -26,4 +28,19 @@ llm = ChatDeepSeek(
 #     api_key=OPEN_API_KEY,
 #     base_url=OPEN_API_URL
 # )
+
+#速率限制
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=0.1,  # 每秒请求数 10秒请求1次
+    check_every_n_seconds=0.1,  #100ms 检查一次是否允许发出请求
+    max_bucket_size=10, #控制最大的突发请求数
+)
+
+model = init_chat_model(
+    model_name="deepseek-r1-0528",
+    model_provider="openai",
+    api_key=ALIBABA_API_KEY,
+    base_url=ALIBABA_BASE_URL,
+    rate_limiter=rate_limiter
+)
 
--- test_llm/test_02.py
+++ test_llm/test_02.py
@@ -1,5 +1,10 @@
 from test import llm
 
+# for chunk in llm.stream("请用三句话介绍 yourself"):
+#     print(type(chunk))
+#     print(chunk)
+
+full = None
 for chunk in llm.stream("请用三句话介绍 yourself"):
-    print(type(chunk))
-    print(chunk)
+    full = chunk if full is None else full + chunk
+    print(full.text)
